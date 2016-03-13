3bd26a40b465e6003bc306def36e97bf0d829ae7
Saif Islam <towfiqul.islam87@gmail.com>
2016-03-13 21:37:22 +0100

    router and views until chap-16

--- content/app/app.js
+++ content/app/app.js
@@ -1,28 +1,69 @@
-var app = angular.module('myapp', []);
+var app = angular.module('myapp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'content/home.html'
+        })
+        .when('/list', {
+            templateUrl: 'content/directory.html',
+            controller: 'mycon1'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+
+}]);
 
 app.controller('mycon1', function($scope) {
 
+    $scope.recordRemove = function(record) {
 
-	$scope.records = [
+        var removedRecord = $scope.records.indexOf(record);
+        $scope.records.splice(removedRecord, 1);
+    };
 
-	{
-		name: "Aparupa",
-		belt: "sexy",
-		rate: 2000
-	},
+    $scope.addRecord = function() {
 
-	{
-		name: "Saif",
-		belt: "shit",
-		rate: 1000
-	},
+        $scope.records.push({
 
-	{
-		name: "Chotku",
-		belt: "cool",
-		rate: 5000
-	}
+            name: $scope.newrecord.name,
+            belt: $scope.newrecord.belt,
+            color: $scope.newrecord.color,
+            rate: $scope.newrecord.rate
 
-	];
+        });
+
+        $scope.newrecord.name = "";
+        $scope.newrecord.belt = "";
+        $scope.newrecord.color = "";
+        $scope.newrecord.rate = "";
+    };
+
+    $scope.records = [
+
+        {
+            name: "Aparupa",
+            belt: "sexy",
+            color: "red",
+            rate: 2000
+        },
+
+        {
+            name: "Saif",
+            belt: "shit",
+            color: "blue",
+            rate: 1000
+        },
+
+        {
+            name: "Chotku",
+            belt: "cool",
+            color: "green",
+            rate: 5000
+        }
+
+    ];
 
 });
